cd25927364fca99630371900d8119d3e129ab823
Rishivikram N <rishi.vikram.1@gmail.com>
2022-05-21 16:35:21 +0530

    Removed eslint rule exclusions

--- src/interfaces/bumblebee_error.ts
+++ src/interfaces/bumblebee_error.ts
@@ -2,7 +2,6 @@ import { errorCodes, statusCodes } from '../libs/constants';
 
 export interface BumblebeeErrorType {
   message: string;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   errorObject?: any;
   errorCode?: errorCodes;
   statusCode?: statusCodes;
