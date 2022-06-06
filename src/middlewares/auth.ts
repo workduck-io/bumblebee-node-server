c9e3ced35ca19359f77241aac76f9552fe49ee18
Rishivikram N <rishi.vikram.1@gmail.com>
2022-06-06 12:31:08 +0530

    Added catch blocks for auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -9,15 +9,18 @@ async function Auth(
 ): Promise<void> {
   const token = req.headers.authorization || req.query?.token?.toString();
 
-  if (token) {
-    const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY) as any;
-    console.log({ decoded });
+  try {
+    if (token) {
+      const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY) as any;
 
-    if (decoded.userName) next();
-  } else {
-    res
-      .status(statusCodes.FORBIDDEN)
-      .send('<p>Protected Page, login to view</p>');
+      if (decoded.userName) next();
+    } else {
+      res
+        .status(statusCodes.UNAUTHORIZED)
+        .send('<p>Protected Page, login to view</p>');
+    }
+  } catch (error) {
+    res.status(statusCodes.UNAUTHORIZED).json({ message: error.toString() });
   }
 }
 
